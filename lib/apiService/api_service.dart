b40633acf22320f4f2f9e6971da3d8abea6004a5
najmulmyself <najmul.myself@gmail.com>
2022-10-10 18:41:26 +0600

    data null shown

--- lib/apiService/api_service.dart
+++ lib/apiService/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 
 class ApiService {
-  late List<dynamic> postData = [];
+  final List<dynamic> postData = [];
 
   Future getData() async {
     final response =
@@ -20,6 +20,8 @@ class ApiService {
 
     // print(a.body);
     // a.map((e) => postData.add(a.id));
+  final List<dynamic> postData = [];
+
     data
         .map(
           (e) => postData.add(
@@ -30,6 +32,6 @@ class ApiService {
     print(postData.length);
     print(postData[0].title);
     // print('heres testdata: ${postData[0]}');
-    // return postData; // return a;
+    return postData; // return a;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,85 @@
-import 'package:api_practice/screen/home_page.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+// import 'package:api_practice/screen/home_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get_navigation/get_navigation.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Material App',
-      home: HomePage(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       title: 'Material App',
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+
+import 'dart:async';  
+import 'dart:convert';  
+  
+import 'package:api_practice/modal/posts.dart';
+import 'package:flutter/material.dart';  
+import 'package:http/http.dart' as http;  
+  
+void main() => runApp(MyApp());  
+  
+class MyApp extends StatefulWidget {  
+  // MyApp({Key key}) : super(key: key);  
+  
+  @override  
+  _MyAppState createState() => _MyAppState();  
+}  
+  
+class _MyAppState extends State<MyApp> {  
+Future<Posts> ? post;  
+  
+  @override  
+  void initState() {  
+    super.initState();  
+    post = fetchPost();  
+  }  
+  
+  @override  
+  Widget build(BuildContext context) {  
+    return MaterialApp(  
+      title: 'Flutter REST API Example',  
+      theme: ThemeData(  
+        primarySwatch: Colors.green,  
+      ),  
+      home: Scaffold(  
+        appBar: AppBar(  
+          title: Text('Flutter REST API Example'),  
+        ),  
+        body: Center(  
+          child: FutureBuilder<Posts>(  
+            future: post,  
+            builder: (context, abc) {  
+              // if (abc.hasData) {  
+              //   return Text(abc.data!.title!);  
+              // } else if (abc.hasError) {  
+              //   return Text("Error shown here : ${abc.error}");  
+              // }  
+                return Text(abc.data!.title!);  
+  
+              // By default, it show a loading spinner.  
+              return CircularProgressIndicator();  
+            },  
+          ),  
+        ),  
+      ),  
+    );  
+  }  
+}  
+  
+Future<Posts> fetchPost() async {  
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));  
+  
+  if (response.statusCode == 200) {  
+    // If the call to the server was successful (returns OK), parse the JSON.  
+    return Posts.fromJson(json.decode(response.body));  
+  } else {  
+    // If that call was not successful (response was unexpected), it throw an error.  
+    throw Exception('Failed to load post');  
+  }  
+}  
