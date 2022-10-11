629b89063bbccefbb5119c41bd426c88e05b27c7
najmulmyself <najmul.myself@gmail.com>
2022-10-11 14:51:25 +0600

    added codex

--- lib/apiService/api_service.dart
+++ lib/apiService/api_service.dart
@@ -1,37 +1,69 @@
-import 'dart:convert';
+// import 'dart:convert';
 
+// import 'package:api_practice/modal/posts.dart';
+// import 'package:http/http.dart' as http;
+
+
+// class ApiService {
+//   final List<dynamic> postData = [];
+
+//   Future getData() async {
+//     final response =
+//         await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+//     // print('${response.body}');
+//     final data = jsonDecode(response.body);
+//     // print(data[0]['title']);
+//     // Posts a = Posts.fromJson(data.toList());
+//     // return Posts.fromJson(data[0]);
+//     // print("hello 1 :${a.title}");
+//     // print("hello 2 :${a.body}");
+
+//     // print(a.body);
+//     // a.map((e) => postData.add(a.id));
+//   final List<dynamic> postData = [];
+
+//     data
+//         .map(
+//           (e) => postData.add(
+//             Posts.fromJson(e),
+//           ),
+//         )
+//         .toList();
+//     print(postData.length);
+//     print(postData[0].title);
+//     // print('heres testdata: ${postData[0]}');
+//     return postData; // return a;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:api_demo/models/posts.dart';
 import 'package:api_practice/modal/posts.dart';
 import 'package:http/http.dart' as http;
 
+class RemoteService {
+  Future<List<Posts>?> getPosts() async {
+    var client = http.Client();
 
-class ApiService {
-  final List<dynamic> postData = [];
-
-  Future getData() async {
-    final response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-    // print('${response.body}');
-    final data = jsonDecode(response.body);
-    // print(data[0]['title']);
-    // Posts a = Posts.fromJson(data.toList());
-    // return Posts.fromJson(data[0]);
-    // print("hello 1 :${a.title}");
-    // print("hello 2 :${a.body}");
-
-    // print(a.body);
-    // a.map((e) => postData.add(a.id));
-  final List<dynamic> postData = [];
-
-    data
-        .map(
-          (e) => postData.add(
-            Posts.fromJson(e),
-          ),
-        )
-        .toList();
-    print(postData.length);
-    print(postData[0].title);
-    // print('heres testdata: ${postData[0]}');
-    return postData; // return a;
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postsFromJson(json);
+    }
   }
 }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,82 +1,196 @@
-import 'dart:convert';
+// import 'dart:convert';
 
+// import 'package:api_practice/apiService/api_service.dart';
+// import 'package:api_practice/modal/posts.dart';
+// import 'package:api_practice/screen/post_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     dynamic testdata = ApiService().getData();
+//     // print('heres testdata: $testdata');
+//     // print('heres testdata print: ${testdata.toString()}');
+//     // print(runtimeType.testdata);
+//     // print(testdata.title);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Material App Bar'),
+//       ),
+//       // body: Center(
+//       //   child: Column(
+//       //     children: [
+//       //       Text('{getData()}'),
+//       //       ElevatedButton(
+//       //           child: Text('Get Data'),
+//       //           onPressed: () {
+//       //             getData();
+//       //             Get.to(PostPage(), transition: Transition.cupertino);
+//       //           }),
+//       //     ],
+//       //   ),
+//       // ),
+//       //////////////
+//       ///
+//       /// FETCHED DATA THROUGH FUTURE BUILDER
+//       ////////////////////////
+//       ///
+//       // body: FutureBuilder(
+//       //     builder: (context, AsyncSnapshot snapshot) {
+//       //       return snapshot.hasData ? Column(
+//       //         children: [
+//       //           Text('${snapshot.data.title}'),
+//       //           // ElevatedButton(
+//       //           //     child: Text('Get Data'),
+//       //           //     onPressed: () {
+//       //           //       getData();
+//       //           //       Get.to(PostPage(), transition: Transition.cupertino);
+//       //           //     }),
+//       //         ],
+//       //       ): Center(child: CircularProgressIndicator(),);
+//       //     },
+//       //     future: getData()),
+
+//       body: ApiService().postData.length != 0
+//           ? ListView.builder(
+//               itemBuilder: (context, index) {
+//                 print(ApiService().postData.length);
+//                 return ListTile(
+//                   title: Text('${ApiService().postData[index].title}'),
+//                   leading: CircleAvatar(),
+//                 );
+//               },
+//               itemCount: ApiService().postData.length,
+//             )
+//           : Center(
+//               child: CircularProgressIndicator(),
+//             ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:api_demo/models/post.dart';
+// import 'package:api_demo/services/remote_service.dart';
 import 'package:api_practice/apiService/api_service.dart';
 import 'package:api_practice/modal/posts.dart';
-import 'package:api_practice/screen/post_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Posts>? posts;
+  var isLoaded = false;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    dynamic testdata = ApiService().getData();
-    // print('heres testdata: $testdata');
-    // print('heres testdata print: ${testdata.toString()}');
-    // print(runtimeType.testdata);
-    // print(testdata.title);
+
+    //fetch data from API
+    getData();
+  }
+
+  getData() async {
+    posts = await RemoteService().getPosts();
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Material App Bar'),
+        title: const Text('Posts'),
       ),
-      // body: Center(
-      //   child: Column(
-      //     children: [
-      //       Text('{getData()}'),
-      //       ElevatedButton(
-      //           child: Text('Get Data'),
-      //           onPressed: () {
-      //             getData();
-      //             Get.to(PostPage(), transition: Transition.cupertino);
-      //           }),
-      //     ],
-      //   ),
-      // ),
-      //////////////
-      ///
-      /// FETCHED DATA THROUGH FUTURE BUILDER
-      ////////////////////////
-      ///
-      // body: FutureBuilder(
-      //     builder: (context, AsyncSnapshot snapshot) {
-      //       return snapshot.hasData ? Column(
-      //         children: [
-      //           Text('${snapshot.data.title}'),
-      //           // ElevatedButton(
-      //           //     child: Text('Get Data'),
-      //           //     onPressed: () {
-      //           //       getData();
-      //           //       Get.to(PostPage(), transition: Transition.cupertino);
-      //           //     }),
-      //         ],
-      //       ): Center(child: CircularProgressIndicator(),);
-      //     },
-      //     future: getData()),
-
-      body: ApiService().postData.length != 0
-          ? ListView.builder(
-              itemBuilder: (context, index) {
-                print(ApiService().postData.length);
-                return ListTile(
-                  title: Text('${ApiService().postData[index].title}'),
-                  leading: CircleAvatar(),
-                );
-              },
-              itemCount: ApiService().postData.length,
-            )
-          : Center(
-              child: CircularProgressIndicator(),
-            ),
+      body: Visibility(
+        visible: isLoaded,
+        // ignore: sort_child_properties_last
+        child: ListView.builder(
+          itemCount: posts?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          posts![index].title!,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          posts![index].body ?? '',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
